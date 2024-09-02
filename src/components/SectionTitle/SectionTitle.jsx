0349700f8cfaf8aa34a8c761b722fde8ee91537e
arafatrahman41 <arafatrahman41@outlook.com>
2024-09-02 08:06:00 +0600

    some debuging

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -2,8 +2,8 @@ import PropTypes from "prop-types";
 
 const SectionTitle = ({ heading, subHeading }) => {
   return (
-    <div className="text-center mx-auto md:w-3/12 mt-16 mb-8">
-      <p className="text-yellow-500 mb-2"> ---{subHeading} ---</p>
+    <div className="text-center mx-auto md:w-4/12 mt-16 mb-8">
+      <p className="text-yellow-500 mb-2"> --- {subHeading} ---</p>
       <p className="text-3xl uppercase border-y-4 py-4">{heading}</p>
     </div>
   );
