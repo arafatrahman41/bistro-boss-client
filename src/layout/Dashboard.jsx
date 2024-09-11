968671c8e583490fb80c6a2e8ab883b642418c23
arafatrahman41 <arafatrahman41@outlook.com>
2024-09-12 02:03:09 +0600

    working

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,33 +1,58 @@
 import { FaHome, FaList, FaShoppingCart } from "react-icons/fa";
 import { FaCalendar } from "react-icons/fa6";
-import { MdReviews } from "react-icons/md";
+import { MdOutlineRestaurantMenu, MdReviews } from "react-icons/md";
 import { NavLink, Outlet } from "react-router-dom";
+import useCart from "../hooks/useCart";
 
 const Dashboard = () => {
+  const [cart] = useCart();
   return (
     <div className="flex">
-        {/* dashboard side bar */}
+      {/* dashboard side bar */}
       <div className="w-64 min-h-screen bg-orange-400">
         <ul className="menu p-4">
           <li>
-            <NavLink to="/dashboard/userHome"><FaHome /> User Home</NavLink>
+            <NavLink to="/dashboard/userHome">
+              <FaHome /> User Home
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/dashboard/reservation"><FaCalendar /> Reservation</NavLink>
+            <NavLink to="/dashboard/reservation">
+              <FaCalendar /> Reservation
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/dashboard/cart"><FaShoppingCart /> My Cart</NavLink>
+            <NavLink to="/dashboard/cart">
+              <FaShoppingCart /> My Cart ({cart.length})
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/dashboard/review"><MdReviews /> Add a Review</NavLink>
+            <NavLink to="/dashboard/review">
+              <MdReviews /> Add a Review
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/dashboard/booking"><FaList /> My Bookings</NavLink>
+            <NavLink to="/dashboard/booking">
+              <FaList /> My Bookings
+            </NavLink>
+          </li>
+          <div className="divider"></div>
+          <li>
+            <NavLink to="/">
+              <FaHome />
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/order/salad">
+              {" "}
+              <MdOutlineRestaurantMenu /> Menu
+            </NavLink>
           </li>
         </ul>
       </div>
       {/* dashboard content */}
-      <div className="flex-1">
+      <div className="flex-1 p-8">
         <Outlet />
       </div>
     </div>
